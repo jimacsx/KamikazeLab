e8d92fdf68166f3291ecc060b7d8a95c4af48ba2
jimacsx <jimacsx.github.com>
2015-11-03 19:10:59 -0600

    ahora si la llamada a cada función animar_x() es dentro del animate del div con id cuadro6

--- rectangulos/eventos.js
+++ rectangulos/eventos.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-	var $contenedor = $('#contenedor');
 	var $cuadro1 = $('#cuadro1');
 	var $cuadro2 = $('#cuadro2');
 	var $cuadro3 = $('#cuadro3');
@@ -11,438 +10,438 @@ $(document).ready(function(){
 	var tiempo = 1000;
 
 	function animar_1(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 0,
 			left: 100 +'px',
 			width: 200 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 100 +'px',
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 0,
 			width: 400 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_2);
 	}
 
 	function animar_2(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 200 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 200 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 200 +'px',
 			left: 100 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 0,
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 200 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_3);
 	}
 
 	function animar_3(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 400 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 100 +'px',
 			left: 100 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 100 +'px',
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 0,
 			width: 400 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_4);
 	}
 
 	function animar_4(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 0,
 			left: 100 +'px',
 			width: 200 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 100 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 100 +'px',
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 200 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_5);
 	}
 
 	function animar_5(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 100 +'px',
 			height: 300 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 0,
 			left: 100 +'px',
 			width: 100 +'px',
 			height: 300 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 0,
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 300 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_6);
 	}
 
 	function animar_6(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 300 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 0,
 			width: 300 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_7);
 	}
 
 	function animar_7(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 0,
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 0,
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_8);
 	}
 
 	function animar_8(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 300 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 300 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 100 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 100 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_9);
 	}
 
 	function animar_9(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 100 +'px',
 			left: 0,
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 100 +'px',
 			left: 100 +'px',
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 0,
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 200 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
-		}, tiempo, 'swing');
+		}, tiempo, 'swing', animar_10);
 	}
 
 	function animar_10(){
-		$contenedor.children('#cuadro1').animate({
+		$cuadro1.animate({
 			top: 0,
 			left: 0,
 			width: 200 +'px',
 			height: 300 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro2').animate({
+		$cuadro2.animate({
 			top: 0,
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro3').animate({
+		$cuadro3.animate({
 			top: 0,
 			left: 300 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro4').animate({
+		$cuadro4.animate({
 			top: 100 +'px',
 			left: 200 +'px',
 			width: 200 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro5').animate({
+		$cuadro5.animate({
 			top: 200 +'px',
 			left: 200 +'px',
 			width: 100 +'px',
 			height: 100 +'px'
 		}, tiempo, 'swing');
 
-		$contenedor.children('#cuadro6').animate({
+		$cuadro6.animate({
 			top: 200 +'px',
 			left: 300 +'px',
 			width: 100 +'px',
@@ -450,21 +449,8 @@ $(document).ready(function(){
 		}, tiempo, 'swing');
 	}
 
-	function animar() {
-		animar_1();
-		animar_2();
-		animar_3();
-		animar_4();
-		animar_5();
-		animar_6();
-		animar_7();
-		animar_8();
-		animar_9();
-		animar_10();
-	}
-
 	$('#miBoton_animar').click(function() {
-		animar();	
+		animar_1();	
 	});
 
 });
