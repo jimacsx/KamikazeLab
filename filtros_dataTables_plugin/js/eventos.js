65f7899762e4cea855e1cbf6f09869091d9c9546
jimacsx <jimacsx.github.com>
2015-11-26 19:14:18 -0600

    cambiando a usar datos declarados en mi archivo JS (eventos.js)

--- filtros_dataTables_plugin/js/eventos.js
+++ filtros_dataTables_plugin/js/eventos.js
@@ -1,20 +1,27 @@
+var registros = [
+  ["", "Av Aztecas", "San Rafael", "2000", "", "Coacalco", "VIMEX", "10", "botones"]
+];
+
 $(document).ready(function() {
-   var tabla = $('#table_id').DataTable({ //propiedades para modificar contenidos de la tabla
-   	"paging":   true,
-      "ordering": true,
-      "info":     true,
-      "pagingType": "simple_numbers", //tipo de paginacion (numbers, simple, simple_numbers, full, full_numbers)
-      //"scrollY": "200px",
-      scrollY: '50vh',	//scroll vertical
-   	scrollX: true,		//scroll horizontal
-      scrollCollapse: 'true',
-      stateSave: true,	//salva el estado actual de la tabla
 
-      "ajax": 'ajax/datos.json'
-   });
+  $('#tabla_id').DataTable({
+    scrollY: '50vh', //scroll vertical
+    scrollX: true,    //scroll horizontal
+    scrollCollapse: 'true',
+    stateSave: true,  //salva el estado actual de la tabla
 
-   $('#table_id tbody').on('click', 'tr', function () { //muestra msj de alerta al dar click en una celda
-        var dato = tabla.row( this ).data();					// es un arreglo donde los atributos de los tabla son datos dentro del arreglo
-        alert( 'You clicked on '+dato[1]+'\'s row' );
-    } );
+    data: registros,
+    columns: [
+      { title: "Imagen" },
+      { title: "Calle" },
+      { title: "Colonia" },
+      { title: "Precio" },
+      { title: "Tipo" },
+      { title: "Desarrollo" },
+      { title: "Desarrollador" },
+      { title: "Solicitudes" },
+      { title: "Acciones" },
+    ]
+  });
+
 });
