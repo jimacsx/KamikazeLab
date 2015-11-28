da2fec2baff2b0a0ef0029de670e8e0c7c511380
jimacsx <jimacsx.github.com>
2015-11-27 19:17:27 -0600

    implementando ahora una BD más grande de nombre world.sql

--- filtros_dataTables_plugin/js/eventos.js
+++ filtros_dataTables_plugin/js/eventos.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     scrollX: true,    //scroll horizontal
     scrollCollapse: 'true',
     stateSave: true,  //salva el estado actual de la tabla
+    pagingType: "full_numbers",  //numbers, simple, simple_numbers, full, full_numbers
     //para ocupar una BD creada en mysql se utilizan las siguientes lineas
     "processing": true,
     "serverSide": true,
--- filtros_dataTables_plugin/php/script.php
+++ filtros_dataTables_plugin/php/script.php
@@ -1,25 +1,21 @@
 <?php
 
-	$table = 'casas';
+	$table = 'city';
 
-	$primaryKey = 'imagen';
+	$primaryKey = 'ID';
 
 	$columns = array(
-		array('db' => 'imagen', 'dt' => 0),
-		array('db' => 'calle', 'dt' => 1),
-		array('db' => 'colonia', 'dt' => 2),
-		array('db' => 'precio', 'dt' => 3),
-		array('db' => 'tipo', 'dt' => 4),
-		array('db' => 'desarrollo', 'dt' => 5),
-		array('db' => 'desarrollador', 'dt' => 6),
-		array('db' => 'solicitudes', 'dt' => 7),
-		array('db' => 'acciones', 'dt' => 8)
+		array('db' => 'ID', 'dt' => 0),
+		array('db' => 'Name', 'dt' => 1),
+		array('db' => 'CountryCode', 'dt' => 2),
+		array('db' => 'District', 'dt' => 3),
+		array('db' => 'Population', 'dt' => 4)
 	);
 
 	$sql_details = array(
 		'user' => 'root',
 		'pass' => '',
-		'db' => 'tucanton',
+		'db' => 'world',
 		'host' => 'localhost'
 	);
 
@@ -29,4 +25,5 @@
 	echo json_encode(
 		SSP::simple($_GET, $sql_details, $table, $primaryKey, $columns)
 	);
+
 ?>
