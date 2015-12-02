fc45aa00252754fd321574959731eb3df03b6c63
jimacsx <jimacsx.github.com>
2015-12-02 12:16:36 -0600

    filtrando búsquedas (mostrando registros de 10 en 10) utilizando BD de tucanton.sql y plugin dataTables

--- filtros_dataTables_plugin/js/eventos.js
+++ filtros_dataTables_plugin/js/eventos.js
@@ -1,17 +1,15 @@
-var registros =[["", "Av Aztecas", "San Rafael", "2000", "", "Coacalco", "VIMEX", "10", "Botones"] ];
-
 $(document).ready(function() {
-
+    
   $('#tabla_id').DataTable({
-    scrollY: '50vh', //scroll vertical
+    scrollY: '100vh', //scroll vertical
     scrollX: true,    //scroll horizontal
     scrollCollapse: 'true',
     stateSave: true,  //salva el estado actual de la tabla
     pagingType: "full_numbers",  //numbers, simple, simple_numbers, full, full_numbers
     //para ocupar una BD creada en mysql se utilizan las siguientes lineas
-    "processing": true,
-    "serverSide": true,
-    "ajax": 'php/script.php'
+    "bProcessing": true,
+    "bServerSide": true,
+    "sAjaxSource": 'php/desarrolladores.php'
   });
 
 });
--- filtros_dataTables_plugin/php/script.php
+++ filtros_dataTables_plugin/php/script.php
@@ -1,21 +1,26 @@
 <?php
 
-	$table = 'city';
+	$table = 'desarrollador';
 
-	$primaryKey = 'ID';
+	$primaryKey = 'iddesarrollador';
 
 	$columns = array(
-		array('db' => 'ID', 'dt' => 0),
-		array('db' => 'Name', 'dt' => 1),
-		array('db' => 'CountryCode', 'dt' => 2),
-		array('db' => 'District', 'dt' => 3),
-		array('db' => 'Population', 'dt' => 4)
+		array('db' => 'iddesarrollador', 'dt' => 0),
+		array('db' => 'nombreDesarrollador', 'dt' => 1),
+		array('db' => 'nombreContacto', 'dt' => 2),
+		array('db' => 'telefonoContacto', 'dt' => 3),
+		array('db' => 'correoContacto', 'dt' => 4),
+		array('db' => 'prioridad', 'dt' => 5),
+		array('db' => 'logo', 'dt' => 6),
+		array('db' => 'direccionFiscal', 'dt' => 7),
+		array('db' => 'rfc', 'dt' => 8),
+		array('db' => 'comision', 'dt' => 9)
 	);
 
 	$sql_details = array(
 		'user' => 'root',
 		'pass' => '',
-		'db' => 'world',
+		'db' => 'tucanton',
 		'host' => 'localhost'
 	);
 
